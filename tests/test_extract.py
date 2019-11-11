b400b473fc08e7bf5857d71253ab4009a29a935a
Viktor Persson <viktor.persson@arcsin.se>
2019-11-12 00:45:49 +0100

    Update tests.

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -16,6 +16,28 @@ class ExtractTest(unittest.TestCase):
         ext = extract.Extractor(TEST_FILE_PATH)
         self.assertEqual(ext.range("A1"), [["name"]])
 
+    def test_range_horizontal(self):
+        ext = extract.Extractor(TEST_FILE_PATH)
+        self.assertEqual(ext.range("A1:B1"), [["name", "age"]])
+
+    def test_range_vertical(self):
+        ext = extract.Extractor(TEST_FILE_PATH)
+        self.assertEqual(ext.range("A1:A4"), [["name"], ["alpha"], ["beta"], ["gamma"]])
+
+    def test_range_square(self):
+        ext = extract.Extractor(TEST_FILE_PATH)
+        self.assertEqual(ext.range("A3:B4"), [["beta", 28], ["gamma", 32]])
+
+    def test_range_multiple_rows(self):
+        ext = extract.Extractor(TEST_FILE_PATH)
+        self.assertEqual(ext.range("1:2"), [["name", "age"], ["alpha", 15]])
+
+    @unittest.skip
+    def test_range_single_row(self):
+        ext = extract.Extractor(TEST_FILE_PATH)
+        self.assertEqual(ext.range("1"), [["name", "age"]])
+        self.assertEqual(ext.range("2:2"), [["alpha", 15]])
+
     # TODO
     @unittest.skip
     def test_range_single_column(self):
@@ -30,39 +52,3 @@ class ExtractTest(unittest.TestCase):
             ext.range("A:B"),
             [["name", "age"], ["alpha", 15], ["beta", 28], ["gamma", 32]],
         )
-
-    def test_range_horizontal(self):
-        ext = extract.Extractor(TEST_FILE_PATH)
-        self.assertEqual(ext.range("A1:B1"), [["name", "age"]])
-
-    def test_range_vertical(self):
-        ext = extract.Extractor(TEST_FILE_PATH)
-        self.assertEqual(ext.range("A1:A4"), [["name"], ["alpha"], ["beta"], ["gamma"]])
-
-    def test_range_square(self):
-        ext = extract.Extractor(TEST_FILE_PATH)
-        self.assertEqual(ext.range("A3:B4"), [["beta", 28], ["gamma", 32]])
-
-    # TODO
-
-    def ztest_rows_single(self):
-        ext = extract.Extractor(TEST_FILE_PATH)
-        self.assertEqual(ext.rows(1), [["name", "age"]])
-        self.assertEqual(ext.rows([1]), [["name", "age"]])
-
-    def ztest_rows_multiple(self):
-        ext = extract.Extractor(TEST_FILE_PATH)
-        self.assertEqual(ext.rows(1, 3), [["name", "age"], ["beta", 28]])
-        self.assertEqual(ext.rows([1, 3]), [["name", "age"], ["beta", 28]])
-
-    def ztest_columns_single(self):
-        ext = extract.Extractor(TEST_FILE_PATH)
-        self.assertEqual(ext.columns([1]), [["name", "age"]])
-        self.assertEqual(ext.columns(["name"]), [["name", "age"]])
-
-    def ztest_columns_multiple(self):
-        ext = extract.Extractor(TEST_FILE_PATH)
-        self.assertEqual(ext.columns(1, 2), [["name", "age"], ["beta", 28]])
-        self.assertEqual(ext.columns([1, 2]), [["name", "age"], ["beta", 28]])
-        self.assertEqual(ext.columns("name", "age"), [["name", "age"], ["beta", 28]])
-        self.assertEqual(ext.columns(["name", "age"]), [["name", "age"], ["beta", 28]])
