1e47360d8fffb8420720bdd95ff87b6eb7cdb61d
Sourcery AI <>
2023-03-09 06:36:36 +0000

    'Refactored by Sourcery'

--- exex/parse.py
+++ exex/parse.py
@@ -10,24 +10,18 @@ def values(val):
     if isinstance(val, Cell):
         return from_cell(val)
     elif isinstance(val, tuple):
-        if isinstance(val[0], Cell):
-            return from_array(val)
-        else:
-            return from_range(val)
+        return from_array(val) if isinstance(val[0], Cell) else from_range(val)
     else:
         return val
 
 
 def from_cell(cell):
-    if type(cell) is Cell:
-        return cell.value
-    else:
-        return cell
+    return cell.value if type(cell) is Cell else cell
 
 
 def from_array(row):
-    return list(from_cell(cell) for cell in list(row))
+    return [from_cell(cell) for cell in list(row)]
 
 
 def from_range(range_):
-    return list([from_array(row) for row in list(range_)])
+    return [from_array(row) for row in list(range_)]
